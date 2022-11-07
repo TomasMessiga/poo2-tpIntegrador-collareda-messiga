5d0bf5a2816c5895fcf2339adeffc508650e1545
tomas <tomasmessiga@uca.edu.ar>
2022-11-07 19:33:21 -0300

    TDD iteration

--- Test/localesDestinos.test.js
+++ Test/localesDestinos.test.js
@@ -1,7 +1,7 @@
 //const Mapa=require('../Prototipos/Mapa.js');
 
 test("definir local",()=>{
-    let nombre=1;
+    let nombre=0;
     let local=new Local(nombre);
     expect(local.nombre).toStrictEqual(nombre)
 })
