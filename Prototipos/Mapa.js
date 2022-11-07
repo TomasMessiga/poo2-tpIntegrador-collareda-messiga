a62e938b94736084281d85aabd6c341dfbb9419a
Juancollareda <114444562+Juancollareda@users.noreply.github.com>
2022-11-07 20:19:52 -0300

    TDD iteration

--- Prototipos/Mapa.js
+++ Prototipos/Mapa.js
@@ -2,7 +2,7 @@
 function Mapa(){
     this.mapa=[["cola de salida de locas"],["Facturación"],["Calidad"] , ["Distribución"]]
     this.generar=function(num){
-        this.mapa.forEach(element => {for(var i=0 ; i<=num+1;i++){element.push()}});
+        this.mapa.forEach(element => {for(var i=0 ; i<num+1;i++){element.push()}});
 
     return this.mapa
 
