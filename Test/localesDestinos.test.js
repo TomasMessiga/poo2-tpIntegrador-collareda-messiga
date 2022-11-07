f0110d3303272354be74dfdf233b058e730ffc75
tomas <tomasmessiga@uca.edu.ar>
2022-11-07 20:23:45 -0300

    TDD iteration

--- Test/localesDestinos.test.js
+++ Test/localesDestinos.test.js
@@ -18,8 +18,23 @@ test("local producir 1 paquete",()=>{
     let local=new Local(nombre);
     expect(local.producirPaquetes()).toStrictEqual(1);
 })
+test("local producir 1 paquete (alternativo)",()=>{
+    let nombre=0;
+    let local=new Local(nombre);
+    expect(local.producirPaquetes(1)).toStrictEqual(1);
+})
 
 test("local producir 2 paquetes",()=>{
+    let nombre=0;
+    let local=new Local(nombre);
+    expect(local.producirPaquetes(2)).toStrictEqual(2);
+})
+test("local producir 3 paquetes",()=>{
+    let nombre=0;
+    let local=new Local(nombre);
+    expect(local.producirPaquetes(2)).toStrictEqual(2);
+})
+test("local producir 4 paquetes",()=>{
     let nombre=0;
     let local=new Local(nombre);
     expect(local.producirPaquetes(2)).toStrictEqual(2);
