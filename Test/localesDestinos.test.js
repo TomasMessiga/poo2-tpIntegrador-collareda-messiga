43d16c38b3383f303899574c4d9d24da5a047920
tomas <tomasmessiga@uca.edu.ar>
2022-11-07 19:39:47 -0300

    TDD iteration

--- Test/localesDestinos.test.js
+++ Test/localesDestinos.test.js
@@ -1,7 +1,13 @@
+const { test } = require('picomatch');
 const Local=require('../Prototipos/Local.js');
 
 test("definir local",()=>{
     let nombre=0;
     let local=new Local(nombre);
     expect(local.nombre).toStrictEqual(nombre)
+})
+test("definir destino",()=>{
+    let nombre="a";
+    let destino=new Destino(nombre);
+    expect(destino.nombre).toStrictEqual(nombre)
 })
