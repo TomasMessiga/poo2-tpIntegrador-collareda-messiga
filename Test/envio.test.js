c978f3b00e4299978b542971065ffc953fc9bdf0
Juancollareda <114444562+Juancollareda@users.noreply.github.com>
2022-11-07 20:08:59 -0300

    TDD iteration

--- Test/envio.test.js
+++ Test/envio.test.js
@@ -8,5 +8,5 @@ test("generar matriz",()=>{
 test("generar matriz con cant de locales",()=>{
     let mapacondestino=new Mapa();
 
-    expect(mapacondestino.generar(1)).toBe([["cola de salida de locas",],["Facturación",],["Calidad",] , ["Distribución",]])
+    expect(mapacondestino.generar(1)).toStrictEqual([["cola de salida de locas",],["Facturación",],["Calidad",] , ["Distribución",]])
 })
