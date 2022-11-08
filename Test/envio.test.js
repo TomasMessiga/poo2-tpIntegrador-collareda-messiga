2663bfa4693d757d9cf9832fd89066071e6311dc
Juancollareda <114444562+Juancollareda@users.noreply.github.com>
2022-11-08 19:08:22 -0300

    TDD iteration

--- Test/envio.test.js
+++ Test/envio.test.js
@@ -2,7 +2,6 @@ const Mapa=require('../Prototipos/Mapa.js');
 
 test("generar matriz",()=>{
     let mapa=new Mapa();
-
     expect(mapa.generar(local,destino)).toStrictEqual([["cola de salida de locas",0],["Facturación",0],["Calidad",0] , ["Distribución",0]])
 })
 
