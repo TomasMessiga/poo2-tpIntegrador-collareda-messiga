a22474a017e9642b52ba478bfcd1f16568d1cbf1
tomas <tomasmessiga@uca.edu.ar>
2022-11-08 19:28:50 -0300

    TDD iteration

--- Prototipos/Local.js
+++ Prototipos/Local.js
@@ -1,3 +1,4 @@
+const { fuchsia } = require('color-name');
 const Paquete=require('../Prototipos/Paquete.js');
 
 
@@ -11,8 +12,24 @@ function Local(nombre){
         return new Paquete(especificaciones[0],especificaciones[1],especificaciones[2]);
     }
 
+    
+    const cantidadCorrecta=(cantidad)=>{
+        return (cantidad<=5 && cantidad>0);
+    }
+
+const fabricarPaquetes=function(cantidad,especificaciones){
+    let cantidadEspecifica={1:[1],2:[1,1],3:[1,1,1],4:[1,1,1,1],5:[1,1,1,1,1]};
+    let arregloAuxiliar=cantidadEspecifica[cantidad];
+    for (let i=0;i<cantidad && arregloAuxiliar!=undefined;i++){
+        arregloAuxiliar[i]=(crearUnPaquete(especificaciones));
+    };
+    return arregloAuxiliar;
+};
+
+
 
     this.producirPaquetes=(cantidad,especificaciones)=>{
+        return (cantidadCorrecta(cantidad)) ? (fabricarPaquetes(cantidad,especificaciones)):(undefined);
         let cantidadEspecifica={1:[1],2:[1,1],3:[1,1,1],4:[1,1,1,1],5:[1,1,1,1,1]};
         let arregloAuxiliar=cantidadEspecifica[cantidad];
         for (let i=0;i<cantidad && arregloAuxiliar!=undefined;i++){
