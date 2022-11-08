ee8db252ab9fcee09c30ba41b5d2e24cb9b8e8b6
tomas <tomasmessiga@uca.edu.ar>
2022-11-08 19:29:14 -0300

    TDD iteration

--- Prototipos/Local.js
+++ Prototipos/Local.js
@@ -17,8 +17,8 @@ function Local(nombre){
         return (cantidad<=5 && cantidad>0);
     }
 
-const fabricarPaquetes=function(cantidad,especificaciones){
-    let cantidadEspecifica={1:[1],2:[1,1],3:[1,1,1],4:[1,1,1,1],5:[1,1,1,1,1]};
+    const fabricarPaquetes=function(cantidad,especificaciones){
+        let cantidadEspecifica={1:[1],2:[1,1],3:[1,1,1],4:[1,1,1,1],5:[1,1,1,1,1]};
     let arregloAuxiliar=cantidadEspecifica[cantidad];
     for (let i=0;i<cantidad && arregloAuxiliar!=undefined;i++){
         arregloAuxiliar[i]=(crearUnPaquete(especificaciones));
@@ -30,12 +30,7 @@ const fabricarPaquetes=function(cantidad,especificaciones){
 
     this.producirPaquetes=(cantidad,especificaciones)=>{
         return (cantidadCorrecta(cantidad)) ? (fabricarPaquetes(cantidad,especificaciones)):(undefined);
-        let cantidadEspecifica={1:[1],2:[1,1],3:[1,1,1],4:[1,1,1,1],5:[1,1,1,1,1]};
-        let arregloAuxiliar=cantidadEspecifica[cantidad];
-        for (let i=0;i<cantidad && arregloAuxiliar!=undefined;i++){
-            arregloAuxiliar[i]=(crearUnPaquete(especificaciones));
-        };
-        return arregloAuxiliar;
+ 
     };
 }
 
