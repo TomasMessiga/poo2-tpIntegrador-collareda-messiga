0b070da25f0a0b8ecabeb58f2fa57cc944e2b3b5
Juancollareda <114444562+Juancollareda@users.noreply.github.com>
2022-11-08 19:08:06 -0300

    TDD iteration

--- Test/envio.test.js
+++ Test/envio.test.js
@@ -3,20 +3,6 @@ const Mapa=require('../Prototipos/Mapa.js');
 test("generar matriz",()=>{
     let mapa=new Mapa();
 
-    expect(mapa.generar()).toStrictEqual([["cola de salida de locas"],["Facturación"],["Calidad"] , ["Distribución"]])
-})
-test("generar matriz con cant de locales",()=>{
-    let mapacondestino=new Mapa();
-
-
-    expect(mapacondestino.generar(1)).toStrictEqual([["cola de salida de locas",0],["Facturación",0],["Calidad",0] , ["Distribución",0]])
-    
-})
-test("generar matriz con cant mas de 1  de locales",()=>{
-    let mapacondestino=new Mapa(); 
-
-
-    expect(mapacondestino.generar(4)).toStrictEqual([["cola de salida de locas",0,0,0,0],["Facturación",0,0,0,0],["Calidad",0,0,0,0] , ["Distribución",0,0,0,0]])
-    
+    expect(mapa.generar(local,destino)).toStrictEqual([["cola de salida de locas",0],["Facturación",0],["Calidad",0] , ["Distribución",0]])
 })
 
