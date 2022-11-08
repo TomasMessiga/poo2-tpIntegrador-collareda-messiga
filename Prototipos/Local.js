cbacd433912a81523b3d4eed77bc0b1ecb5805e0
tomas <tomasmessiga@uca.edu.ar>
2022-11-08 01:34:38 -0300

    TDD iteration

--- Prototipos/Local.js
+++ Prototipos/Local.js
@@ -8,7 +8,7 @@ function Local(nombre){
      // hay que insertar en el arreglo los paquetes o se repite el paquete insertado es el mismo
     this.producirPaquetes=(cantidad,paquete)=>{
         let mapeo=function(){return paquete.crearReplica();};
-        let cantidadCorrespondiente={1:[0],2:[0,0],3:[0,0,0],4:[0,0,0,0],5:[0,0,0,0,0]};
+        let cantidadCorrespondiente={1:[1],2:[1,1],3:[1,1,1],4:[1,1,1,1],5:[1,1,1,1,1]};
         let retorno=(cantidadCorrespondiente[cantidad]);
         return retorno.map();
     };
