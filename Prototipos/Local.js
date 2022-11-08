031e08edc7919ceb986aba397daed70ae6059f4d
tomas <tomasmessiga@uca.edu.ar>
2022-11-08 19:26:49 -0300

    TDD iteration

--- Prototipos/Local.js
+++ Prototipos/Local.js
@@ -11,27 +11,15 @@ function Local(nombre){
         return new Paquete(especificaciones[0],especificaciones[1],especificaciones[2]);
     }
 
-    const cantidadCorrecta=(cantidad)=>{
-        return (cantidad<=5 && cantidad>0);
-    }
-    const fabricarPaquetes=function(cantidad,especificaciones){
-        
-        let cantidadEspecifica={1:[1],2:[1,1],3:[1,1,1],4:[1,1,1,1],5:[1,1,1,1,1]};
-        let arregloAuxiliar=cantidadEspecifica[cantidad];
-        arregloAuxiliar.forEach(value,index => {
-            value=crearUnPaquete(especificaciones);
-        });console.log(arregloAuxiliar)
-    }
 
     this.producirPaquetes=(cantidad,especificaciones)=>{
-        return (cantidadCorrecta) ? fabricarPaquetes(cantidad,especificaciones) : undefined;
-
-
-    //     for (let i=0;i<cantidad && arregloAuxiliar!=undefined;i++){
-    //        arregloAuxiliar[i]=(crearUnPaquete(especificaciones));
-    //    };
-      //  
-    }
+        let cantidadEspecifica={1:[1],2:[1,1],3:[1,1,1],4:[1,1,1,1],5:[1,1,1,1,1]};
+        let arregloAuxiliar=cantidadEspecifica[cantidad];
+        for (let i=0;i<cantidad && arregloAuxiliar!=undefined;i++){
+            arregloAuxiliar[i]=(crearUnPaquete(especificaciones));
+        };
+        return arregloAuxiliar;
+    };
 }
 
 module.exports=Local;
