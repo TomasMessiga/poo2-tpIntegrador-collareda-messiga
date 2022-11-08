02049e3566582e1eb1b7f83f9a1171681bc87a09
tomas <tomasmessiga@uca.edu.ar>
2022-11-08 01:51:46 -0300

    TDD iteration

--- Prototipos/Local.js
+++ Prototipos/Local.js
@@ -8,10 +8,9 @@ function Local(nombre){
      // hay que insertar en el arreglo los paquetes o se repite el paquete insertado es el mismo
     this.producirPaquetes=(cantidad,paquete)=>{
         let cantidadCorrespondiente={1:[1],2:new Array(1,1),3:new Array(1,1,1),4:new Array(1,1,1,1),5:new Array(1,1,1,1,1)};
-        let retorno=[];
-        retorno.concat(cantidadCorrespondiente[cantidad])
+        let retorno=cantidadCorrespondiente[cantidad];
         
-        retorno.forEach(valor,indice=>{
+        retorno.forEach(valor=>{
        //     retorno[indice]=paquete.crearReplica();
         });
         return retorno;
