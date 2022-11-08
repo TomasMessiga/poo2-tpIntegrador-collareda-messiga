7f20ba998f3bc979a565d4d8af43a758b4418fa8
tomas <tomasmessiga@uca.edu.ar>
2022-11-08 19:05:23 -0300

    TDD iteration

--- Prototipos/Local.js
+++ Prototipos/Local.js
@@ -15,7 +15,7 @@ function Local(nombre){
     this.producirPaquetes=(cantidad,especificaciones)=>{
         let cantidadEspecifica={1:[1],2:[1,1],3:[1,1,1],4:[1,1,1,1],5:[1,1,1,1,1]};
         let arregloAuxiliar=cantidadEspecifica[cantidad];
-        arregloAuxiliar.forEach(element,index => {
+        arregloAuxiliar.foreach(e,index,a => {
             
         });
         for (let i=0;i<cantidad && arregloAuxiliar!=undefined;i++){
