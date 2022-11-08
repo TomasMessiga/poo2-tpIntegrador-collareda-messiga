d9e80357bf06a3ab848748e39a670b1c3100c5ed
Juancollareda <114444562+Juancollareda@users.noreply.github.com>
2022-11-08 19:12:46 -0300

    TDD iteration

--- Test/Mapatest.test.js
+++ Test/Mapatest.test.js
@@ -1,5 +1,6 @@
 const Mapa=require('../Prototipos/Mapa.js');
-
+const Local=require('../Prototipos/Local.js');
+const Destino=require('../Prototipos/Destino.js');
 test("generar matriz",()=>{
     let mapa=new Mapa();
     expect(mapa.generar(local,destino)).toStrictEqual([["cola de salida de locas",0],["Facturación",0],["Calidad",0] , ["Distribución",0]])
