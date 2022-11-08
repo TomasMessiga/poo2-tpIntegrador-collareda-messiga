ead7eb815d370d27200a4255e2214a2fe0bef1a1
tomas <tomasmessiga@uca.edu.ar>
2022-11-08 18:34:10 -0300

    TDD iteration

--- Prototipos/Local.js
+++ Prototipos/Local.js
@@ -14,7 +14,6 @@ function Local(nombre){
         for (let i=0;i<cantidad;i++){
             arregloAuxiliar[i]=crearReplicaDePaquete(paquete);
         }
-  //      let cantidadCorrespondiente={1:[paquete],2:[paquete,paquete],3:[paquete,paquete,paquete],4:[paquete,paquete,paquete,paquete],5:[paquete,paquete,paquete,paquete,paquete]};
         return arregloAuxiliar;
     };
 }
