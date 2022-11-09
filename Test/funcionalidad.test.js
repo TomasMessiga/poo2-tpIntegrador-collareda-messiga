8dac55ac6fffae0eac4562f85d963a158d4cac41
Alumno <collaredajuan@uca.edu.ar>
2022-11-09 20:30:56 -0300

    TDD iteration

--- Test/funcionalidad.test.js
+++ Test/funcionalidad.test.js
@@ -1,5 +1,7 @@
 const Sistema=require('../Prototipos/Sistema.js');
 const Mapa=require('../Prototipos/Mapa.js');
+const Local=require('../Prototipos/Local.js');
+const Destino=require('../Prototipos/Destino.js'); 
 
 test("incrementar una unidad de tiempo",()=>{
     let sistema=new Sistema();
@@ -12,4 +14,11 @@ test("insertar un mapa en el sistema",()=>{
     let mapaAuxiliar=new Mapa();
     sistema.mapa=mapaAuxiliar.generar();
     expect(sistema.mapa).toStrictEqual(mapaAuxiliar.generar());
+})
+test("mover paquete",()=>{
+    let sistema=new Sistema();
+    let mapaAuxiliar=new Mapa();
+
+    sistema.mapa=mapaAuxiliar.generar(Local,Destino)
+    expect().toStrictEqual(mapaAuxiliar.generar());
 })
