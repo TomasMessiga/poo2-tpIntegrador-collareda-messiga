7941af7e585f38cb5030a8f04e7c153aea409d49
tomas <tomasmessiga@uca.edu.ar>
2022-11-09 19:59:24 -0300

    TDD iteration

--- Test/funcionalidad.test.js
+++ Test/funcionalidad.test.js
@@ -2,5 +2,5 @@
 test("incrementar una unidad de tiempo",()=>{
     let sistema=new Sistema();
     sistema.incrementarTiempo();
-    expect(sistema.getTiempo()).toBe(1);
+    expect(sistema.getTiempo()).toBe(01);
 })
