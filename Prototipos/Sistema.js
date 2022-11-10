a06fabdbf266fa8b2d64cdf7733044c99ff64c75
Alumno <collaredajuan@uca.edu.ar>
2022-11-10 17:49:22 -0300

    TDD iteration

--- Prototipos/Sistema.js
+++ Prototipos/Sistema.js
@@ -17,7 +17,7 @@ function Sistema(){
                      if(this.mapa[i][1]==paquete && this.mapa[i][0]!="Distribución" && bandera==1){
                         bander=0
                         this.mapa[i][1]=0
-                        this.mapa[2][1]=paquete
+                        this.mapa[i+1][1]=paquete
 
                      }
 
