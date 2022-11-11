296b9d32797385f8088a2bd9bae12a86869946b7
Juancollareda <114444562+Juancollareda@users.noreply.github.com>
2022-11-11 19:02:12 -0300

    TDD iteration

--- Prototipos/Sistema.js
+++ Prototipos/Sistema.js
@@ -14,7 +14,7 @@ function Sistema(){
     {       
         let index=[];
         mapa.mapa.forEach(element => {
-            index.push(element[0][1])     
+            index.push(element[2])     
         });
        
         return index
