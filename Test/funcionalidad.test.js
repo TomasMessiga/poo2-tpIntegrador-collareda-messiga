8c349d2dab3fb08b63292ecfbc848d98b7a83b87
Juancollareda <114444562+Juancollareda@users.noreply.github.com>
2022-11-11 16:52:49 -0300

    TDD iteration

--- Test/funcionalidad.test.js
+++ Test/funcionalidad.test.js
@@ -16,23 +16,3 @@ test("insertar un mapa en el sistema",()=>{
     sistema.mapa=mapaAuxiliar.generar();
     expect(sistema.mapa).toStrictEqual(mapaAuxiliar.generar());
 })
-test("mover paquete",()=>{
-    let sistema=new Sistema();
-    let mapaAuxiliar=new Mapa() ;
-    let destino=new Destino();
-    let local= new Local();
-    let paquete= new Paquete();
-    sistema.mapa=mapaAuxiliar.generar(local,destino)
-
-    expect(sistema.mover(paquete)).toStrictEqual([["cola de salida de locas",paquete],["Facturación",0],["Calidad",0] , ["Distribución",0]]);
-})
-test("mover paquete",()=>{
-    let sistema=new Sistema();
-    let mapaAuxiliar=new Mapa() ;
-    let destino=new Destino();
-    let local= new Local();
-    let paquete= new Paquete();
-    sistema.mapa=mapaAuxiliar.generar(Local,Destino)
-    sistema.mover(paquete)
-    expect(sistema.mover(paquete)).toStrictEqual([["cola de salida de locas",0],["Facturación",Paquete],["Calidad",0] , ["Distribución",0]]);
-})
