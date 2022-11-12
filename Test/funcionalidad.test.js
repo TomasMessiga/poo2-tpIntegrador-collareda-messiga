66c793236a08e0dd0a6ca55589d0b1761cb22d43
Juancollareda <114444562+Juancollareda@users.noreply.github.com>
2022-11-11 23:35:35 -0300

    TDD iteration

--- Test/funcionalidad.test.js
+++ Test/funcionalidad.test.js
@@ -9,6 +9,18 @@ test("incrementar una unidad de tiempo",()=>{
     sistema.incrementarTiempo();
     expect(sistema.getTiempo()).toBe(1);
 })
+test("mover 1 pasos pauqete en mapa",()=>{
+    let sistema=new Sistema();
+    let mapaAuxiliar=new Mapa();
+    let local=new Local('a')
+    let destino=new Destino(0)
+    let paquete=new Paquete(1,1,1,1);
+    mapaAuxiliar.generar(local,destino);
+    
+    sistema.mover(mapaAuxiliar,paquete)
+    sistema.mover(mapaAuxiliar,paquete)
+    expect(mapaAuxiliar.mapa).toStrictEqual([["cola de salida de locas",[]],["Facturación",[paquete]],["Calidad",[]] , ["Distribución",[]]]);
+})
 test("mover 2 pasos pauqete en mapa",()=>{
     let sistema=new Sistema();
     let mapaAuxiliar=new Mapa();
