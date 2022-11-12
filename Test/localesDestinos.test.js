aac7447d6c54795838dd955947b4026221bb7edb
tomas <tomasmessiga@uca.edu.ar>
2022-11-12 18:29:42 -0300

    TDD iteration

--- Test/localesDestinos.test.js
+++ Test/localesDestinos.test.js
@@ -124,15 +124,14 @@ test("locales con produccion simultanea",()=>{
     let destinoPaquetes=[];
     destinoPaquetes.concat(a.producirPaquete([1,1,1]));
     destinoPaquetes.concat(a.producirPaquete([1,1,1]));
+    destinoPaquetes.concat(b.producirPaquete([1,1,1]));
+    destinoPaquetes.concat(b.producirPaquete([1,1,1]));
+    destinoPaquetes.concat(b.producirPaquete([1,1,1]));
+    destinoPaquetes.concat(b.producirPaquete([1,1,1]));
+    destinoPaquetes.concat(b.producirPaquete([1,1,1]));
+    destinoPaquetes.concat(b.producirPaquete([1,1,1]));
     destinoPaquetes.concat(c.producirPaquete([1,1,1]));
     destinoPaquetes.concat(c.producirPaquete([1,1,1]));
-    destinoPaquetes.concat(c.producirPaquete([1,1,1]));
-    destinoPaquetes.concat(b.producirPaquete([1,1,1]));
-    destinoPaquetes.concat(b.producirPaquete([1,1,1]));
-    destinoPaquetes.concat(b.producirPaquete([1,1,1]));
-    destinoPaquetes.concat(b.producirPaquete([1,1,1]));
-    destinoPaquetes.concat(b.producirPaquete([1,1,1]));
-    destinoPaquetes.concat(b.producirPaquete([1,1,1]));
-   
-    expect(a.mostrarDisponibilidad()-b.mostrarDisponibilidad()-1).toStrictEqual(b.mostrarDisponibilidad());
+
+    expect(a.mostrarDisponibilidad()).toStrictEqual(b.mostrarDisponibilidad()+c.mostrarDisponibilidad());
 })
