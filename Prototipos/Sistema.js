6fb8e8499d53b071cc6ed5354702c220dfa4434a
Juancollareda <114444562+Juancollareda@users.noreply.github.com>
2022-11-12 19:55:19 -0300

    TDD iteration

--- Prototipos/Sistema.js
+++ Prototipos/Sistema.js
@@ -12,7 +12,7 @@ function Sistema(){
     this.getTiempo=function(){return tiempo;}
     this.mover=function (mapa,paquete){
       aux=mapa.buscar(paquete)
-      if(aux[0]==0 && aux[0]==1 && aux[2]==0){
+      if(aux[0]==0 && aux[1]==0 && aux[2]==0){
 
         mapa.mapa[aux[0]][aux[0]].push(paquete)
 
