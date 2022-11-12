557aca4843ba66164ceea85fbdf5528aa7967633
Juancollareda <114444562+Juancollareda@users.noreply.github.com>
2022-11-11 23:06:44 -0300

    TDD iteration

--- Test/funcionalidad.test.js
+++ Test/funcionalidad.test.js
@@ -16,6 +16,7 @@ test("insertar pauqete en mapa",()=>{
     let destino=new Destino(0)
     let paquete=new Paquete(1,1,1,1);
     mapaAuxiliar.generar(local,destino);
+    mapaAuxiliar.mapaAuxiliar[0][3].push(paquete)
     
     expect(sistema.mover(mapaAuxiliar,paquete)).toBe(1);
 })
