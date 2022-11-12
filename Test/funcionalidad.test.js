3a69a349cd25ae5524203a0cdecd25971269e702
Juancollareda <114444562+Juancollareda@users.noreply.github.com>
2022-11-12 15:53:17 -0300

    TDD iteration

--- Test/funcionalidad.test.js
+++ Test/funcionalidad.test.js
@@ -19,8 +19,8 @@ test("mover 1 pasos pauqete en mapa",()=>{
     mapaAuxiliar.generar(local,destino);
     
     sistema.mover(mapaAuxiliar,paquete)
-    sistema.mover(mapaAuxiliar,paquete)
-    expect(mapaAuxiliar.mapa).toStrictEqual([["cola de salida de locas",[]],["Facturación",[paquete]],["Calidad",[]] , ["Distribución",[]]]);
+    expect(sistema.getTiempo()).toBe(1);
+    expect(mapaAuxiliar.mapa).toStrictEqual([["cola de salida de locas",[paquete]],["Facturación",[]],["Calidad",[]] , ["Distribución",[]]]);
 })
 test("mover 2 pasos pauqete en mapa",()=>{
     let sistema=new Sistema();
@@ -77,20 +77,4 @@ test("LLego a destino",()=>{
  
 
 
-})
-test(" no puedo mover facturacion",()=>{
-    let sistema=new Sistema();
-    let mapaAuxiliar=new Mapa();
-    let local=new Local('a')
-    let destino=new Destino(0)
-    let paquete=new Paquete(1,1,1,1);
-    let paquete2=new Paquete(1,1,1,2);
-    let paquete3=new Paquete(1,1,1,3);
-    let paquete4=new Paquete(1,1,1,4);
-    mapaAuxiliar.generar(local,destino);
-    mapaAuxiliar.mapa[1][1].push(paquete2)
-    mapaAuxiliar.mapa[1][1].push(paquete3)
-    mapaAuxiliar.mapa[1][1].push(paquete4)    
-    sistema.mover(mapaAuxiliar,paquete)
-    expect(sistema.mover(mapaAuxiliar,paquete)).toStrictEqual([["cola de salida de locas",[paquete]],["Facturación",[paquete2,paquete3,paquete4]],["Calidad",[]] , ["Distribución",[]]]);
 })
