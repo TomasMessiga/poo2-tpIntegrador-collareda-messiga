733bd45680bf2a70820769b44d30efb489d176bf
Juancollareda <114444562+Juancollareda@users.noreply.github.com>
2022-11-12 19:44:34 -0300

    TDD iteration

--- Prototipos/Mapa.js
+++ Prototipos/Mapa.js
@@ -23,17 +23,13 @@ function Mapa(){
                 if (this.mapa[i][j].length>0){
                     for (var k = 0; k < this.mapa[i][j].length; k++){
                         if(this.mapa[i][j][k].id=paquete.id){
-
                             fila=i
                             columna=j
                             poscola=k
                         }
                     }
-                }
-               
-        
+                }             
             }
-        
           }
         return ("fila:"+fila+"columna:"+columna+"Poscola:"+poscola)
             
--- Test/Mapatest.test.js
+++ Test/Mapatest.test.js
@@ -33,5 +33,16 @@ test("buscar pauqete",()=>{
     mapa.mapa[0][1].push(paquete)
     expect(mapa.buscar(paquete)).toStrictEqual("fila:"+0+"columna:"+1+"Poscola:"+0)
 })
+test("buscar pauqete cuando hay 2 paquetes",()=>{
+    let mapa=new Mapa();
+    let local=new Local('a')
+    let destino= new Destino(0)
+    let paquete = new Paquete(1,1,1,1)
+    let paquete1 = new Paquete(1,1,1,2)
+    mapa.generar([local,destino]) 
+    mapa.mapa[0][1].push(paquete2)
+    mapa.mapa[1][1].push(paquete)
+    expect(mapa.buscar(paquete)).toStrictEqual("fila:"+1+"columna:"+1+"Poscola:"+0)
+})
 
 
