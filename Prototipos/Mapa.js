942cc7ed78d847af9c7e6d78908d9eeb4a9a9a75
Juancollareda <114444562+Juancollareda@users.noreply.github.com>
2022-11-12 19:14:57 -0300

    TDD iteration

--- Prototipos/Mapa.js
+++ Prototipos/Mapa.js
@@ -1,3 +1,4 @@
+const fill = require("fill-range");
 
 function Mapa(){ 
     this.mapa=[["cola de salida de locas"],["Facturación"],["Calidad"] , ["Distribución"]]
@@ -9,7 +10,25 @@ function Mapa(){
         
         
         });
-    this.buscarpaquete=function(paquete){
+    this.buscar=function(paquete){
+        fila=0
+        columna=0
+        for (var i = 1; i <= this.mapa.length-1; i++) {
+            for (var j = 0; j <= this.mapa[i].length-1; i++) {
+                
+                if (this.mapa[i]>0){
+
+                    fila=i
+                    columna=j
+                }
+
+        
+            }
+        
+          }
+        return ('fila:'+i+'columna'+j)
+            
+        };
         
 
     }    
