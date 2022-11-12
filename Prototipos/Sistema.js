ee84e3c1f9c1d5079ec64260bb19ef6f9aa83f42
Juancollareda <114444562+Juancollareda@users.noreply.github.com>
2022-11-11 22:01:50 -0300

    TDD iteration

--- Prototipos/Sistema.js
+++ Prototipos/Sistema.js
@@ -11,7 +11,8 @@ function Sistema(){
     }
     this.getTiempo=function(){return tiempo;}
     this.mover=function (mapa,paquete){    
-           return mapa.mapa[0][1].length
+        mapa.mapa[0][1].push(paquete)   
+        return mapa
 }
 }
 module.exports=Sistema;
