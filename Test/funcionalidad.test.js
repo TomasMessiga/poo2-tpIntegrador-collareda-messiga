20643f6f06a02a808e3dcd76b63100a5fbfd4f8e
Juancollareda <114444562+Juancollareda@users.noreply.github.com>
2022-11-11 23:28:16 -0300

    TDD iteration

--- Test/funcionalidad.test.js
+++ Test/funcionalidad.test.js
@@ -20,7 +20,8 @@ test("insertar pauqete en mapa",()=>{
     sistema.mover(mapaAuxiliar,paquete)
     sistema.mover(mapaAuxiliar,paquete)
     sistema.mover(mapaAuxiliar,paquete)
-    expect(mapaAuxiliar.mapa).toStrictEqual([["cola de salida de locas",[paquete]],["Facturación",[paquete]],["Calidad",[paquete]] , ["Distribución",[]]]);
+    sistema.mover(mapaAuxiliar,paquete)
+    expect(mapaAuxiliar.mapa).toStrictEqual([["cola de salida de locas",[paquete]],["Facturación",[paquete]],["Calidad",[paquete]] , ["Distribución",[paquete]]]);
 })
 
 
