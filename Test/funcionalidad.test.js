5de909aabee6b332bd1302632d4b78a0a00bd6fe
Juancollareda <114444562+Juancollareda@users.noreply.github.com>
2022-11-13 17:36:15 -0300

    TDD iteration

--- Test/funcionalidad.test.js
+++ Test/funcionalidad.test.js
@@ -38,11 +38,27 @@ test("mover 2 pasos pauqete en mapa",()=>{
     let local=new Local('a')
     let destino=new Destino(0)
     let paquete=new Paquete(1,1,1,1);
+    let paquete2=new Paquete(2,2,2,2);
     mapaAuxiliar.generar(local,destino);
     sistema.mover(mapaAuxiliar,paquete)
     sistema.mover(mapaAuxiliar,paquete)
     sistema.mover(mapaAuxiliar,paquete)
     expect(mapaAuxiliar.mapa).toStrictEqual([["cola de salida de local","Facturación","Calidad","Distibucion"],[[],[],[paquete],[]]]);
+})
+
+test("mover 2 pasos pauqete en mapa",()=>{
+    let sistema=new Sistema();
+    let mapaAuxiliar=new Mapa();
+    let local=new Local('a')
+    let destino=new Destino(0)
+    let paquete=new Paquete(1,1,1,1);
+    let paquete2=new Paquete(2,2,2,2);
+    mapaAuxiliar.generar(local,destino);
+    sistema.mover(mapaAuxiliar,paquete)
+    sistema.mover(mapaAuxiliar,paquete)
+    sistema.mover(mapaAuxiliar,paquete)
+    sistema.mover(mapaAuxiliar,paquete2)
+    expect(mapaAuxiliar.mapa).toStrictEqual([["cola de salida de local","Facturación","Calidad","Distibucion"],[[paquete2],[],[paquete],[]]]);
 })
 
 
