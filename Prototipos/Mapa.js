9868288c1c60ed9126eb1641c4cf2c4809683e98
Juancollareda <114444562+Juancollareda@users.noreply.github.com>
2022-11-13 17:15:52 -0300

    TDD iteration

--- Prototipos/Mapa.js
+++ Prototipos/Mapa.js
@@ -20,7 +20,7 @@ function Mapa(){
             for (var j = 0; j < this.mapa[i].length; j++) {
                 if (this.mapa[i][j].length>0){
                     for (var k = 0; k < this.mapa[i][j].length; k++){
-                        if(this.mapa[i][j][k].id=paquete.id){
+                        if(this.mapa[i][j][k].id==paquete.id){
                             fila=i
                             columna=j
                             poscola=k
