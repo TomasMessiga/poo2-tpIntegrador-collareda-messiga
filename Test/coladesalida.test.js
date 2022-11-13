4e90fb45f6dfcdb002a81988ef570cc6c4732f2b
Juancollareda <114444562+Juancollareda@users.noreply.github.com>
2022-11-13 19:41:18 -0300

    TDD iteration

--- Test/coladesalida.test.js
+++
@@ -1,8 +0,0 @@
-const ColaDeSalida=require("../Prototipos/Coladesalida")
-const Local=require('../Prototipos/Local.js');
-test("generar colade2salida",()=>{
-    let nombre=0
-    let local=new Local(nombre);
-    let colasalida=new ColaDeSalida();
-    expect(colasalida.generar(local)).toBe("cola de salida de"+local.nombre)
-})
