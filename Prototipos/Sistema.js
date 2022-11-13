a391ad4c88a13a715d749c9de78fc8e993eff177
Juancollareda <114444562+Juancollareda@users.noreply.github.com>
2022-11-13 15:34:50 -0300

    TDD iteration

--- Prototipos/Sistema.js
+++ Prototipos/Sistema.js
@@ -13,7 +13,7 @@ function Sistema(){
       fila=aux[0]
       columna=aux[1]
       poscola=aux[2]
-      cant=mapa[fila].length-1
+      cant=mapa.mapa[fila].length-1
       if(poscola===-1){
         mapa.mapa[fila][columna+cant].push(paquete)
 
