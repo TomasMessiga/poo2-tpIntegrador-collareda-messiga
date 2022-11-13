42eeb0dd1b5765d7c87f87f54a04232b34c5c4ae
Juancollareda <114444562+Juancollareda@users.noreply.github.com>
2022-11-13 17:05:27 -0300

    TDD iteration

--- Test/Mapatest.test.js
+++ Test/Mapatest.test.js
@@ -22,6 +22,24 @@ test("buscar pauqete cuando hay 2 paquetes",()=>{
     mapa.mapa[1][0].push(paquete)
     expect(mapa.buscar(paquete)).toStrictEqual(mapa.mapa[1][0][1])
 })
+test("buscar pauqete cuando hay vario paquetes",()=>{
+    let mapa=new Mapa();
+    let local=new Local('a')
+    let destino= new Destino(0)
+    let paquete = new Paquete(1,1,1,1)
+    let paquete2 = new Paquete(2,2,2,2)
+    let local2=new Local('b')
+    let destino2= new Destino(1)
+    mapa.generar([local2,destino2])
+    mapa.generar([local,destino]) 
+    mapa.mapa[1][0].push(paquete2)
+    mapa.mapa[1][0].push(paquete)
+    mapa.mapa[1][2].push(paquete3)
+
+    expect(mapa.buscar(paquete)).toStrictEqual(mapa.mapa[1][0][1])
+})
+
+
 
 
 
