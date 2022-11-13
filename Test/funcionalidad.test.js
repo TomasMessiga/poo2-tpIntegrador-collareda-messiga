fe240adec86043dc9982a50faad81c56ea68f91b
Juancollareda <114444562+Juancollareda@users.noreply.github.com>
2022-11-13 18:25:13 -0300

    TDD iteration

--- Test/funcionalidad.test.js
+++ Test/funcionalidad.test.js
@@ -45,20 +45,6 @@ test("mover 2 pasos pauqete en mapa",()=>{
     sistema.mover(mapaAuxiliar,paquete)
     expect(mapaAuxiliar.mapa).toStrictEqual([["cola de salida de local","Facturación","Calidad","Distibucion"],[[],[],[paquete],[]]]);
 })
-
-test("mover 2 pasos pauqete en mapa",()=>{
-    let sistema=new Sistema();
-    let mapaAuxiliar=new Mapa();
-    let local=new Local('a')
-    let destino=new Destino(0)
-    let paquete=new Paquete (1,1,1,1)
-    mapaAuxiliar.generar(local,destino);
-    sistema.Corrertiempo(mapaAuxiliar,local,[1,1,1,1])
-    expect(mapaAuxiliar.mapa).toStrictEqual([["cola de salida de local","Facturación","Calidad","Distibucion"],[[paquete],[],[],[]]]);
-})
-
-
-
 test("fabricar paquetes para un mapa",()=>{
     let sistema=new Sistema();
     let mapaAuxiliar=new Mapa();
