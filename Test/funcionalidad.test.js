bf3706b7f68d4051fa38dd7dfdf174fc1fb73c7c
Juancollareda <114444562+Juancollareda@users.noreply.github.com>
2022-11-13 19:02:07 -0300

    TDD iteration

--- Test/funcionalidad.test.js
+++ Test/funcionalidad.test.js
@@ -53,7 +53,8 @@ test("fabricar paquetes para un mapa",()=>{
     let paquete=new Paquete(1,1,1,1);
     mapaAuxiliar.generar(local,destino);
     //sistema.especificarFabricacion('a',mapaAuxiliar,[1,1,1,1]);
-    expect(sistema.especificarFabricacion(mapaAuxiliar.mapa)).toStrictEqual([["cola de salida de local","Facturación","Calidad","Distibucion"],[[paquete],[],[],[]]]);
+    sistema.especificarFabricacion(mapaAuxiliar.mapa)
+    expect(mapa.mapa).toStrictEqual([["cola de salida de local","Facturación","Calidad","Distibucion"],[[paquete],[],[],[]]]);
 })
 
  
