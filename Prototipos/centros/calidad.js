3aadd2086ccc21c769dc3ffdaac738e6b92f413d
Juancollareda <114444562+Juancollareda@users.noreply.github.com>
2022-11-13 21:02:11 -0300

    TDD iteration

--- Prototipos/centros/calidad.js
+++ Prototipos/centros/calidad.js
@@ -1,40 +1,20 @@
 
 function Calidad(){
+    this.MeMuevoA=function(mapa,columna){
+         
+        for (var i=0 ; i<mapa.mapa.length;i++){
 
-    const rangoProcesamiento=[1,1];
-    const rangoColaDeEspera=[2,5];
+            if (mapa.mapa[i][columna+1].length==0){
+                return [i,columna+1]
+
+            }
 
 
-    this.procesamiento=[];
-    this.colaDeEspera=[];
-    this.limiteProcesamiento=rangoProcesamiento[0];
-    this.limiteColaDeEspera=rangoColaDeEspera[0];
 
-    const limitesAdecuados=function(arreglo,limites){
-        let longitud=arreglo.length;
-        return (longitud>=limites[0] && longitud<=limites[1]);
-    };
-
-    
-    this.determinarLimites=function(procesamiento,cola){
-        if (limitesAdecuados(this.procesamiento,rangoProcesamiento)){
-            this.limiteProcesamiento=procesamiento;
-        } if (limitesAdecuados(this.colaDeEspera,rangoColaDeEspera)){
-            this.limiteColaDeEspera=cola;
         }
-    }
+        return [0,0]
 
-    this.incorporarProcesamiento=function(paquete){
-        if (this.procesamiento.length<=this.limiteProcesamiento){
-            this.procesamiento.push(paquete);
-        }
     }
-    this.incorporarColaDeEspera=function(paquete){
-        if (this.colaDeEspera.length<=this.limiteColaDeEspera){
-            this.colaDeEspera.push(paquete);
-        }
-    }
-
 
 
 }
--- Prototipos/centros/distribucion.js
+++ Prototipos/centros/distribucion.js
@@ -1,40 +1,21 @@
 
 function Distribucion(){
 
-    const rangoProcesamiento=[1,10];
-    const rangoColaDeEspera=[10,30];
+    this.MeMuevoA=function(mapa,columna){
+         
+        for (var i=0 ; i<mapa.mapa.length;i++){
+
+            if (mapa.mapa[i][columna+1].length==0){
+                return [i,columna+1]
+
+            }
 
 
-    this.procesamiento=[];
-    this.colaDeEspera=[];
-    this.limiteProcesamiento=rangoProcesamiento[0];
-    this.limiteColaDeEspera=rangoColaDeEspera[0];
 
-    const limitesAdecuados=function(arreglo,limites){
-        let longitud=arreglo.length;
-        return (longitud>=limites[0] && longitud<=limites[1]);
-    };
-
-    
-    this.determinarLimites=function(procesamiento,cola){
-        if (limitesAdecuados(this.procesamiento,rangoProcesamiento)){
-            this.limiteProcesamiento=procesamiento;
-        } if (limitesAdecuados(this.colaDeEspera,rangoColaDeEspera)){
-            this.limiteColaDeEspera=cola;
         }
-    }
+        return [0,0]
 
-    this.incorporarProcesamiento=function(paquete){
-        if (this.procesamiento.length<=this.limiteProcesamiento){
-            this.procesamiento.push(paquete);
-        }
     }
-    this.incorporarColaDeEspera=function(paquete){
-        if (this.colaDeEspera.length<=this.limiteColaDeEspera){
-            this.colaDeEspera.push(paquete);
-        }
-    }
-
 
 }
 module.exports=Distribucion;
