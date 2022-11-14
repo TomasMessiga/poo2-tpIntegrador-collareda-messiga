72db65b1978ea0536195f0fb102b719934b9077e
tomas <tomasmessiga@uca.edu.ar>
2022-11-14 19:26:10 -0300

    TDD iteration

---
+++ Prototipos/MapaAuxiliar.js
@@ -0,0 +1,17 @@
+
+function Mapa(){ 
+    this.mapa=[];
+    var limites=[0,0];
+    this.locales=[];
+    this.destinos=[];
+    this.determinarLimites=function(filas,columnas){
+        limites=[filas,columnas];
+    }
+
+
+
+    }    
+
+
+
+module.exports=Mapa;
