c830427d7a954126d19db5782ba2fdabaaf04ca2
tomas <tomasmessiga@uca.edu.ar>
2022-11-14 17:41:39 -0300

    TDD iteration

--- Test/Errores.test.js
+++ Test/Errores.test.js
@@ -3,5 +3,7 @@ const Validacion=require('../Prototipos/Validacion.js');
 test('Recibir un error',()=>{
     let causa=new Validacion();
     let mensaje="Cero no es igual que uno";
-    expect(causa.verificarCumplimiento(0==1,mensaje)).toThrow(new Error(mensaje));
+    expect(() => {
+        (causa.verificarCumplimiento(0==1,mensaje))
+      }).toThrow(new Error(mensaje));
 })
