b660e65b95a84a2d0b811c2f70e7c8dacf31773b
Alumno <juancollareda@uca.edu.ar>
2022-11-14 15:25:26 -0300

    TDD iteration

--- Prototipos/Sistema.js
+++ Prototipos/Sistema.js
@@ -50,7 +50,7 @@ function Sistema(){
 
     }
     
-     paquete=paquete.sort((a,b) => a.id - b.id);
+     paquete=paquete.sort((a,b) => b.id - a.id);
 
     for (var i=0 ; i<paquete.length;i++){
 
