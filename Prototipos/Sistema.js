213f76ee32ffd6bddb25b055481872e65d449fcd
Juancollareda <114444562+Juancollareda@users.noreply.github.com>
2022-11-13 21:54:05 -0300

    TDD iteration

--- Prototipos/Sistema.js
+++ Prototipos/Sistema.js
@@ -40,10 +40,10 @@ function Sistema(){
     }
     this.avanzartiempo=function(mapa){
       let aux=mapa.buscartodos()
-      aux=[]
+      aux=0
       for (var i=0 ; i<aux.length;i++){
 
-        arr.push(mapa.mapa[aux[i][0]][aux[i][1]][aux[i][2]])
+        aux=mapa.mapa[aux[i][0]][aux[i][1]][aux[i][2]]
 
     }
      return aux
