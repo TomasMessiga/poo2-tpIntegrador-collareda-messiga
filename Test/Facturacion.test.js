780e50b5236d5245c91fb0b0a0f9880f989d6f0b
Juancollareda <114444562+Juancollareda@users.noreply.github.com>
2022-11-13 23:05:28 -0300

    TDD iteration

--- Test/Facturacion.test.js
+++ Test/Facturacion.test.js
@@ -26,8 +26,8 @@ test('prueba de fabrica',()=>{
     sistema.mover(mapaAuxiliar,paquete2)
     sistema.mover(mapaAuxiliar,paquete3)
     sistema.mover(mapaAuxiliar,paquete4)
-    sistema.mover(mapaAuxiliar,paquete2)
-    sistema.mover(mapaAuxiliar,paquete3)
+    //sistema.mover(mapaAuxiliar,paquete2)
+    //sistema.mover(mapaAuxiliar,paquete3)
     sistema.mover(mapaAuxiliar,paquete4)
 
    //[[coladesalida,facturacion,calidad,distrbucion],[[paquete2],[paquete],[],[]]]
