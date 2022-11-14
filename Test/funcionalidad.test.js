6a0bfa18086b3f964ae7dd4fa04d4e32c67bcc44
Juancollareda <114444562+Juancollareda@users.noreply.github.com>
2022-11-13 22:48:36 -0300

    TDD iteration

--- Test/funcionalidad.test.js
+++ Test/funcionalidad.test.js
@@ -53,7 +53,7 @@ test("mover 2 pasos pauqete en mapa",()=>{
     sistema.especificarFabricacion('a',mapaAuxiliar,[1,1,1,1])
     sistema.especificarFabricacion('a',mapaAuxiliar,[2,2,2,2]);
     //sistema.mover(paquete)//[[coladesalida,facturacion,calidad,distrbucion],[[paquete2],[paquete],[],[]]]
-    expect(sistema.avanzartiempo(mapaAuxiliar)).toStrictEqual();
+    expect(this.mover(paquete2)).toStrictEqual();
 })
 test("fabricar paquetes para un mapa",()=>{
     let sistema=new Sistema();
