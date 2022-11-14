6583eef1e9c47e8b35d4b854fcef24ba478b8ea4
Juancollareda <114444562+Juancollareda@users.noreply.github.com>
2022-11-13 23:11:58 -0300

    TDD iteration

--- Prototipos/centros/facturacion.js
+++ Prototipos/centros/facturacion.js
@@ -4,7 +4,7 @@ function Facturacion(){
          
         for (var i=0 ; i<mapa.mapa.length;i++){
 
-            if (mapa.mapa[i][columna+1].length==0 && mapa.mapa[i][columna+1].length<=3){
+            if (mapa.mapa[i][columna+1].length==0 || mapa.mapa[i][columna+1].length<=3){
                 return [i,columna+1]
 
             }
