d6a6ebdd4a72b49cdc24b8a62a27a31a6840f180
tomas <tomasmessiga@uca.edu.ar>
2022-11-14 19:00:49 -0300

    TDD iteration

--- Prototipos/centros/calidad.js
+++ Prototipos/centros/calidad.js
@@ -9,10 +9,13 @@ function Calidad(){
     this.limiteProcesamiento=rangoProcesamiento[0];
     this.limiteColaDeEspera=rangoColaDeEspera[0];
 
-    const limitesAdecuados=function(arreglo,limites){
-        let longitud=arreglo.length;
-        return (longitud>=limites[0] && longitud<=limites[1]);
-    };
+    this.determinarLimites=function(procesamiento,cola){
+        if (limitesAdecuados(this.procesamiento,rangoProcesamiento)){
+            this.limiteProcesamiento=procesamiento;
+        } if (limitesAdecuados(this.colaDeEspera,rangoColaDeEspera)){
+            this.limiteColaDeEspera=cola;
+        }
+    }
 
 
     this.MeMuevoA=function(mapa,columna,fila){
