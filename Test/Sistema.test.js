1464822541313c2cd4ef57b9c3d29575cf54c944
tomas <tomasmessiga@uca.edu.ar>
2022-11-14 20:41:56 -0300

    TDD iteration

--- Test/Sistema.test.js
+++ Test/Sistema.test.js
@@ -38,5 +38,5 @@ test("crear un mapa (insertar centros)",()=>{
     mapa.determinarLimites(1,4);
     mapa.definir();
     mapa.agregarCentroParaMapas("facturacion");
-    expect(mapa.mapa[0][1]).toStrictEqual(facturacion);
+    expect(mapa.mapa[0][1]).toStrictEqual(new Facturacion());
 })
