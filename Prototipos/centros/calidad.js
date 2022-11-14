caf8eacaae62943ecb14c7631d852bf7c4bb4255
Alumno <juancollareda@uca.edu.ar>
2022-11-14 15:35:30 -0300

    TDD iteration

--- Prototipos/centros/calidad.js
+++ Prototipos/centros/calidad.js
@@ -4,7 +4,7 @@ function Calidad(){
          
         for (var i=0 ; i<mapa.mapa.length;i++){
 
-            if (mapa.mapa[i][columna+1].length==0 || mapa.mapa[i][columna+1].length<=1){
+            if (mapa.mapa[i][columna+1].length==0 || mapa.mapa[i][columna+1].length<1){
                 return [i,columna+1]
 
             }
