3e0c20133da6e31f34cbcc2610a1cf828f5f6593
Juancollareda <114444562+Juancollareda@users.noreply.github.com>
2022-11-13 21:38:21 -0300

    TDD iteration

--- Test/funcionalidad.test.js
+++ Test/funcionalidad.test.js
@@ -102,7 +102,7 @@ test("avanzar tiempo pero 4 paquete 1 queda en cola de salida",()=>{
     //sistema.especificarFabricacion('a',mapaAuxiliar,[4,4,4,4]);
     aux=sistema.avanzartiempo(mapaAuxiliar)
     
-    expect(aux[0]).toStrictEqual();
+    expect(aux).toStrictEqual();
 })
  
  
