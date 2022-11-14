7e9d15a65ca67600ee47d8400e1acd3c04c43c05
Juancollareda <114444562+Juancollareda@users.noreply.github.com>
2022-11-13 23:23:44 -0300

    TDD iteration

--- Prototipos/centros/distribucion.js
+++ Prototipos/centros/distribucion.js
@@ -5,7 +5,7 @@ function Distribucion(){
          
         for (var i=0 ; i<mapa.mapa.length;i++){
 
-            if (mapa.mapa[i][columna+1].length==0 && mapa.mapa[i][columna+1].length<=10 ){
+            if (mapa.mapa[i][columna+1].length==0 || mapa.mapa[i][columna+1].length<=10 ){
                 return [i,columna+1]
 
             }
