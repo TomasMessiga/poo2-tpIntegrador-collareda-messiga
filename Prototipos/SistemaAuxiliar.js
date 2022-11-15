e74ed4dd98c3ea97fe713e60705c9fe31c935ff0
tomas <tomasmessiga@uca.edu.ar>
2022-11-15 15:42:01 -0300

    TDD iteration

--- Prototipos/SistemaAuxiliar.js
+++ Prototipos/SistemaAuxiliar.js
@@ -61,16 +61,17 @@ function Sistema(){
       if (!(mapa===undefined)){        
         mapa.incrementarTiempoPaquetes();
         for (let i=0;i<mapa.getLimites()[0];i++){
-          for (let f=mapa.getLimites()[1];f>=0;f--){
+          for (let f=mapa.getLimites()[1]-1;f>=0;f--){
             mapa.mapa[i][f].cola.sort(function(a,b){
               return estadoUrgencia(b,mapa.mapa[i].length)-estadoUrgencia(a,mapa.mapa[i].length);
             });
             for (let j=0;j<mapa.mapa[i][f].cola.length;j++){
-              let paqueteAux=mapa.mapa[i][f].cola[j];
+              let paqueteAux=mapa.mapa[i][f].procesarPaquete(mapa.mapa[i][f].cola[j]);
+
               
               if (proto(paqueteAux)==proto(new Paquete()) && f!=mapa.mapa[i].length-1){
                   mapa.mapa[i][f].cola[j]=undefined;
-                  mapa.mapa[i][f+1].cola[j]=paqueteAux;
+                  mapa.mapa[i][f+1].cola[j]=paqueteAux[0];
 
               } else if (proto(paqueteAux)==proto(new Paquete()) && f==mapa.mapa[i].length-1){
                 mapa.mapa[i][f].cola[j]=undefined;
