fa203602f857339f8a7b5b0eabe09faf283221a5
tomas <tomasmessiga@uca.edu.ar>
2022-11-14 22:15:11 -0300

    TDD iteration

--- Prototipos/SistemaAuxiliar.js
+++ Prototipos/SistemaAuxiliar.js
@@ -25,7 +25,7 @@ function Sistema(){
 
     this.fabricarUnPaquete=function(mapa,nombreLocal,especificaciones){
       let cont=0;
-      mapa.locales.forEach(unLocal,indice,arreglo => {
+      mapa.locales.forEach(unLocal => {
         if (unLocal.nombre==nombreLocal){
           let paquete=unLocal.producirPaquete(especificaciones);
           if (!(paquete===undefined)){
