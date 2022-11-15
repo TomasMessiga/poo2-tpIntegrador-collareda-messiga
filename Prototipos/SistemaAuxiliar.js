3dffab3f96f9abec9bd1db61a0388e290d9bbba6
tomas <tomasmessiga@uca.edu.ar>
2022-11-15 16:15:20 -0300

    TDD iteration

--- Prototipos/SistemaAuxiliar.js
+++ Prototipos/SistemaAuxiliar.js
@@ -74,7 +74,7 @@ function Sistema(){
               if (f==0){
                 let paqueteAux=mapa.mapa[i][f].cola[j];  
                 mapa.mapa[i][f].cola[j]=undefined;
-                mapa.mapa[i][f+1].cola[j].push(paqueteAux);
+                mapa.mapa[i][f+1].cola.push(paqueteAux);
               } else if (f==mapa.mapa[i].length-1){
                 let paqueteAux=mapa.mapa[i][f].procesarPaquete(mapa.mapa[i][f].cola[j]);  
                 mapa.mapa[i][f].cola[j]=undefined;
