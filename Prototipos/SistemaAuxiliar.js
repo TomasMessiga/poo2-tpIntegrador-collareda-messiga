e2546d2710392f1c4d99136452d134b30aff013c
tomas <tomasmessiga@uca.edu.ar>
2022-11-15 01:29:20 -0300

    TDD iteration

--- Prototipos/SistemaAuxiliar.js
+++ Prototipos/SistemaAuxiliar.js
@@ -74,7 +74,7 @@ function Sistema(){
           cont+=columna.colaDeProcesamiento.length;
         });
       });
-      mapa.destinos.forEach(paquetesDestino => {console.log(cont)
+      mapa.destinos.forEach(paquetesDestino => {console.log(paquetesDestino.recibidos.length)
         cont+=paquetesDestino.recibidos.length;
       });
       return cont;
