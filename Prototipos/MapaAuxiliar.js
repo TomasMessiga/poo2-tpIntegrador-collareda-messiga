e558bcaad0246709f29539d901eb7451655585f2
tomas <tomasmessiga@uca.edu.ar>
2022-11-15 19:35:06 -0300

    TDD iteration

--- Prototipos/MapaAuxiliar.js
+++ Prototipos/MapaAuxiliar.js
@@ -142,18 +142,18 @@ function Mapa(){
         var fila=limites[0] 
         var columna=limites[1]
         if (destino-(pos[0]+1)<=columna-pos[0] && destino-(pos[0]-1)<=columna-pos[0] ){
-            return [1,-1]
+            return [1,-1];
         }
         if (destino-(pos[0]+1)<=columna-(pos[0]+1))
         {
-            return [1,0]
+            return [1,0];
         }
         if (destino-(pos[0]-1)<=columna-(pos[0]+1))
         {
-            return [0,-1]
+            return [0,-1];
         }
         else{
-            return [0,0]
+            return [0,0];
         }
 
 
