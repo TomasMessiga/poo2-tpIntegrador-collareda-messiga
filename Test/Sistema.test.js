428fabdd608762e2780cad74eb2378528f30c846
tomas <tomasmessiga@uca.edu.ar>
2022-11-15 15:22:22 -0300

    TDD iteration

--- Test/Sistema.test.js
+++ Test/Sistema.test.js
@@ -236,7 +236,6 @@ test("funcionamiento de sistema (mover de cola de salida a destino)",()=>{
     sistema.fabricarUnPaquete(mapa,"A",[2,"reloj","normal"]);
     sistema.fabricarUnPaquete(mapa,"A",[3,"reloj","rapido"]);
     sistema.incrementarTiempo(mapa);
-    console.log(mapa.mapa[0][1])
     expect(mapa.destinos[0].recibidos.length).toStrictEqual(1);
 })
 
