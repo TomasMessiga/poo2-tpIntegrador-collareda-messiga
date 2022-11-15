71aca744082ac37d5b93bbf19e053ddd7189629f
tomas <tomasmessiga@uca.edu.ar>
2022-11-14 23:21:37 -0300

    TDD iteration

--- Prototipos/SistemaAuxiliar.js
+++ Prototipos/SistemaAuxiliar.js
@@ -12,14 +12,24 @@ function Sistema(){
       mapa.determinarLimites(filas,columnas);
     }
 
+    const filtrarPorUndefined=function(value, index, array) {
+      return !(value===undefined);
+    }
+    
+
     this.incrementarTiempo=function(mapa){
+
       if (!(mapa===undefined)){
         for (let i=0;i<mapa.mapa.length;i++){
           for (let f=0;f<mapa.mapa[i].length;f++){
             for (let j=0;j<mapa.mapa[i][f].colaDeProcesamiento.length;j++){
-              let paqueteAux=mapa.mapa[i][f].colaDeProcesamiento;
-              console.log(paqueteAux)
+              let paqueteAux=mapa.mapa[i][f].colaDeProcesamiento[j];
+              if (!(paqueteAux===undefined)){
+                mapa.mapa[i][f].colaDeProcesamiento[j]=undefined;
+                mapa.mapa[i][f+1].colaDeProcesamiento[j]=paqueteAux;
+              }
             }
+            mapa.mapa[i][f].colaDeProcesamiento=mapa.mapa[i][f].colaDeProcesamiento.filter(filtrarPorUndefined);
           }
         }
         tiempo++;
