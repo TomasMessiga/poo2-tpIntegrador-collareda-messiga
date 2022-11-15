37d75086f747f01e4b7e508aec84565a4525bcd4
tomas <tomasmessiga@uca.edu.ar>
2022-11-15 11:56:29 -0300

    TDD iteration

--- Prototipos/MapaAuxiliar.js
+++ Prototipos/MapaAuxiliar.js
@@ -59,7 +59,7 @@ function Mapa(){
     this.buscarPorID=function(numero){
         for (let i=0;i<this.mapa.length;i++){
             for (let j=0;j<this.mapa[i].length;j++){
-                for (let k=0;k<this.mapa[i][j].cola;k++){
+                for (let k=0;k<this.mapa[i][j].cola.length;k++){
 console.log(this.mapa[i][j].cola);
                 }
                 
