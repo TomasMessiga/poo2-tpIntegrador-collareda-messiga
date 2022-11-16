74f0f15249680493e87ad5b241f2bece23823724
Juancollareda <114444562+Juancollareda@users.noreply.github.com>
2022-11-15 22:28:23 -0300

    TDD iteration

--- Prototipos/MapaAuxiliar.js
+++ Prototipos/MapaAuxiliar.js
@@ -132,7 +132,7 @@ function Mapa(){
         var destino=paquete.destino-1;
         var fila=limites[0] ;
         var columna=limites[1];
-        console.log(fila-destino)
+        console.log(destino)
         if ((fila-destino)-(pos[0]+1)<=columna-pos[0] && (fila-destino)-(pos[0]-1)<=columna-pos[0]){
             return [1,-1];
         }
