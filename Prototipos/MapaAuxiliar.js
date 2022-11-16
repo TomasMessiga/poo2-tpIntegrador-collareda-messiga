f2973fd4da02067c719d429b9055751c0b86eab1
tomas <tomasmessiga@uca.edu.ar>
2022-11-15 21:41:28 -0300

    TDD iteration

--- Prototipos/MapaAuxiliar.js
+++ Prototipos/MapaAuxiliar.js
@@ -120,8 +120,6 @@ function Mapa(){
 
     this.desplazamientoVertical=function(paquete){
         var pos=this.buscarCoordenadasPaquetePorID(paquete.id);
-        console.log(paquete)
-        console.log(paquete.destino)
         var destino=paquete.destino-1
         var fila=limites[0] 
         var columna=limites[1]
