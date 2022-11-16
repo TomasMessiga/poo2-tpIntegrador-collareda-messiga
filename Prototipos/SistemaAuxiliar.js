ec0cbc61c8129d9cea2f3d515444db0d666a9ccd
Juancollareda <114444562+Juancollareda@users.noreply.github.com>
2022-11-16 00:11:26 -0300

    TDD iteration

--- Prototipos/SistemaAuxiliar.js
+++ Prototipos/SistemaAuxiliar.js
@@ -105,7 +105,7 @@ function Sistema(){
                 } if (!prototipo){
                   let paqueteParaCola=mapa.mapa[i][f].cola[j];
                   let vertical=0
-                  if ((f)-(i+1)<=paqueteParaCola[0].destino){
+                  if ((i+1)-(f+1)==paqueteParaCola[0].destino){
                     vertical=-1
                   }            //        console.log(vertical)
                   if(mapa.mapa[i+vertical][f+1].incorporarPaqueteCola(paqueteParaCola[0])){
