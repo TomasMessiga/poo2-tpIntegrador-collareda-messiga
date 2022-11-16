a89ad40f0e5b1fe6c00240471bbf1b79842aae84
tomas <tomasmessiga@uca.edu.ar>
2022-11-15 22:24:34 -0300

    TDD iteration

--- Prototipos/SistemaAuxiliar.js
+++ Prototipos/SistemaAuxiliar.js
@@ -76,9 +76,9 @@ function Sistema(){
               if (f==0){
                 let paqueteAux=mapa.mapa[i][f].cola[j];                  
                 let vertical=mapa.desplazamientoVertical(paqueteAux);console.log(vertical)
-                if(mapa.mapa[i+vertical[0]][f+1].incorporarPaqueteCola(paqueteAux)){
+                if(mapa.mapa[i][f+1].incorporarPaqueteCola(paqueteAux)){
                   mapa.mapa[i][f].cola.splice(j,1);
-                } else if(mapa.mapa[i+vertical[1]][f+1].incorporarPaqueteCola(paqueteAux)){
+                } else if(mapa.mapa[i][f+1].incorporarPaqueteCola(paqueteAux)){
                   mapa.mapa[i][f].cola.splice(j,1);
                 }
               } else if (f==mapa.mapa[i].length-1){
@@ -107,9 +107,9 @@ function Sistema(){
                 } if (!prototipo){
                   let paqueteParaCola=mapa.mapa[i][f].cola[j];
                   let vertical=mapa.desplazamientoVertical(paqueteParaCola[0]);//console.log(vertical)
-                  if(mapa.mapa[i+vertical[0]][f+1].incorporarPaqueteCola(paqueteParaCola[0])){
+                  if(mapa.mapa[i][f+1].incorporarPaqueteCola(paqueteParaCola[0])){
                     mapa.mapa[i][f].cola.splice(j,1);
-                  } else if(mapa.mapa[i+vertical[1]][f+1].incorporarPaqueteCola(paqueteParaCola[0])){
+                  } else if(mapa.mapa[i][f+1].incorporarPaqueteCola(paqueteParaCola[0])){
                     mapa.mapa[i][f].cola.splice(j,1);
                   }
                 }
